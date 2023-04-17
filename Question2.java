6a31db28a9ac98fb0b5c66506409a0412056e513
robinchang1 <130948649+robinchang1@users.noreply.github.com>
2023-04-17 11:08:59 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -12,9 +12,7 @@ public class Question2
      
     Scanner in = new Scanner(System.in);
     double height = in.nextDouble();
-    
-    Scanner in2 = new Scanner(System.in);
-    double weight = in2.nextDouble();
+    double weight = in.nextDouble();
 
     System.out.println(weight / (height * height));
   }
